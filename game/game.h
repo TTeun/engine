02672d0532349e4adbc5a52e80228c7347a0ef93
TTeun <teunverstraaten@gmail.com>
2017-07-09 01:30:36 +0200

    CMake

--- game/game.h
+++ game/game.h
@@ -22,7 +22,4 @@ private:
   bool running = true;
 };
 
-
-
-
 #endif
--- main.cc
+++ main.cc
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <string>
 #include <memory>
 #include "game/game.h"
 
-using namespace std;
-
-int main(){
-  unique_ptr<Game> game(new Game());
+int main(int argc, char **argv)
+try
+{
+  std::unique_ptr<Game> game(new Game());
   game->init_game();
   game->run("assets/levels/level3");
 
+  return 0;
+}
+catch (...)
+{
+  return 1;
 }
